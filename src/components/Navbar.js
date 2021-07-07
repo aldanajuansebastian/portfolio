be5cf99de7203176a9f783de1deae01093bf80b5
aldanajuansebastian <aldanajuansebastian1@gmail.com>
2021-07-07 15:35:49 -0500

    project cinepelis added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ function Navbar () {
         <div className="navbar">
             <motion.div className="itemNav" whileHover={{scale:1.1,}}><a href="./" >Home</a></motion.div>
             <motion.div className="itemNav" whileHover={{scale:1.1,}}><a href="#projects">Projects</a></motion.div>
-            <motion.div className="itemNav" whileHover={{scale:1.1,}}><a href="./">Skills</a></motion.div>
+            <motion.div className="itemNav" whileHover={{scale:1.1,}}><a href="#skills">Skills</a></motion.div>
         </div>
     )
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import './Projects.css';
 import {motion} from 'framer-motion';
+import Cinepelis from './img/cinepelis.png'
 
 
 function Projects () {
     return (
         <div className="projects" id="projects">
            <motion.h1 whileHover= {{scale:0.9}}>Projects</motion.h1>
-
+            <div className="projectslist">
+                <div className="project">
+                    <div className="imgproject"><img src={Cinepelis} alt="Logo Cinepelis" width="100%"></img></div>
+                    <h3>CinePelis</h3>
+                    <p>Project developed with React</p>
+                </div>
+                
+            </div>
 
         </div>
     )
