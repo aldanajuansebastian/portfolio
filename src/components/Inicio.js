ee9417837aa9a4a7293c9462cc4dcd522cd35b68
aldanajuansebastian <aldanajuansebastian1@gmail.com>
2021-07-07 20:31:07 -0500

    css modifications

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -12,15 +12,18 @@ function Inicio () {
             <video autoPlay loop muted className="videobg">
                 <source src={fondo} type="video/mp4"></source>
             </video>
-
-            <div className="imghome">
+            <div className="contenedor">
+                <div className="imghome">
                 <motion.img whileHover={{scale:1.1}} src={imagen} width="100%"></motion.img>
-            </div>
+                </div>
 
-            <motion.h1 whileHover={{scale:1.1}}>Sebastian Aldana</motion.h1>
-            <motion.p whileHover={{scale:1.05}}>Modern Languages Professional</motion.p>
-            <motion.p whileHover={{scale:1.05}}>Web Developer (Frontend)</motion.p>
-            <motion.p whileHover={{scale:1.05}}>Digital Marketing</motion.p>
+                <motion.h1 whileHover={{scale:1.1}}>Sebastian Aldana</motion.h1>
+                <motion.p whileHover={{scale:1.05}}>Modern Languages Professional</motion.p>
+                <motion.p whileHover={{scale:1.05}}>Web Developer (Frontend)</motion.p>
+                <motion.p whileHover={{scale:1.05}}>Digital Marketing</motion.p>
+                
+            </div>
+            
 
             
             
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Projects.css';
 import {motion} from 'framer-motion';
 import Cinepelis from './img/cinepelis.png'
+import Venuspizza from './img/venuspizza.png'
 
 
 function Projects () {
@@ -10,8 +11,13 @@ function Projects () {
            <motion.h1 whileHover= {{scale:0.9}}>Projects</motion.h1>
             <div className="projectslist">
                 <div className="project">
-                    <div className="imgproject"><img src={Cinepelis} alt="Logo Cinepelis" width="100%"></img></div>
+                    <div className="imgproject"><img src={Cinepelis} alt="Cinepelis" width="100%"></img></div>
                     <h3>CinePelis</h3>
+                    <p>Project developed with React</p>
+                </div>
+                <div className="project">
+                    <div className="imgproject"><img src={Venuspizza} alt="VenusPizza" width="100%"></img></div>
+                    <h3>Venus Pizza</h3>
                     <p>Project developed with React</p>
                 </div>
                 
