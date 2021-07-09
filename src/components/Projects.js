4a4b5a8140122c9ef9643de30532b8160eea4cfc
aldanajuansebastian <aldanajuansebastian1@gmail.com>
2021-07-08 19:06:07 -0500

    white added css

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,25 +4,29 @@ import {motion} from 'framer-motion';
 import CinePelis from './img/cinepelis.png';
 import Venuspizza from './img/venuspizza.png';
 import { Link } from "react-router-dom";
+import White from './img/white.png';
 
 
 
 function Projects () {
     return (
-        <div className="projects" id="projects">
-           <motion.h1 whileHover= {{scale:0.9}}>Projects</motion.h1>
-            <div className="projectslist">
-                <Link to="/cinepelis" className="project" style={{ textDecoration: 'none' }}>
-                    <div className="imgproject"><img src={CinePelis} alt="Cinepelis" width="100%"></img></div>
-                    <h3>CinePelis</h3>
-                    <p>Project developed with React</p>
-                </Link>
-                <Link to="/venuspizza" className="project" style={{ textDecoration: 'none' }}>
-                    <div className="imgproject"><img src={Venuspizza} alt="VenusPizza" width="100%"></img></div>
-                    <h3>Venus Pizza</h3>
-                    <p>Project developed with React</p> 
-                </Link>
-                
+        <div>
+            <img className="white" src={White} width="100%" alt="fondo blanco"></img>
+            <div className="projects" id="projects">
+            <motion.h1 whileHover= {{scale:0.9}}>Projects</motion.h1>
+                <div className="projectslist">
+                    <Link to="/cinepelis" className="project" style={{ textDecoration: 'none' }}>
+                        <div className="imgproject"><img src={CinePelis} alt="Cinepelis" width="100%"></img></div>
+                        <h3>CinePelis</h3>
+                        <p>Project developed with React</p>
+                    </Link>
+                    <Link to="/venuspizza" className="project" style={{ textDecoration: 'none' }}>
+                        <div className="imgproject"><img src={Venuspizza} alt="VenusPizza" width="100%"></img></div>
+                        <h3>Venus Pizza</h3>
+                        <p>Project developed with React</p> 
+                    </Link>
+                    
+                </div>
             </div>
         </div>
     )
